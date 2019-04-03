5a974b3ab81874d9ec174feb5145a84e0b077107
Michael Neu <git@michaeln.eu>
2019-04-03 22:15:45 +0200

    add jest

---
+++ backend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  transform: {
+    "^.+\\.ts$": "ts-jest"
+  },
+  testRegex: "/test/.*\\.spec\\.ts$",
+  moduleFileExtensions: [
+    "ts",
+    "js"
+  ]
+}
---
+++ backend/test/tilt.spec.ts
@@ -0,0 +1,5 @@
+describe("tilt", () => {
+  it("works", () => {
+    expect(1).toBe(1);
+  });
+});
