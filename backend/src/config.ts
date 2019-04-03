b07f5ac4dccbb63beb21fe1e1aa820d68d59ac5d
Michael Neu <git@michaeln.eu>
2019-04-03 23:12:11 +0200

    add dotenv

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,4 +1,12 @@
 import * as convict from "convict";
+import * as dotenv from "dotenv";
+
+const envParseResult = dotenv.config();
+
+if (envParseResult.error) {
+  // tslint:disable-next-line: no-console
+  console.warn(`Error loading .env configuration: ${envParseResult.error}`);
+}
 
 interface ITiltConfiguration {
   http: IHttpConfiguration;
