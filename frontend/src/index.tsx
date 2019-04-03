5beb221d9ce178115acf1e4b75300a7cf6019087
Michael Neu <git@michaeln.eu>
2019-04-04 00:04:58 +0200

    add webpack config

---
+++ frontend/src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const container = document.getElementById("app");
+const app = (
+  <h1>It works!</h1>
+);
+
+ReactDOM.render(app, container);
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,3 +1,77 @@
-module.exports = {
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
+const isProduction = process.env.NODE_ENV === "production";
+
+module.exports = {
+  entry: "./src/index.tsx",
+
+  output: {
+    filename: isProduction ? "[name].[contenthash].js" : "[name].[hash].js",
+    chunkFilename: isProduction ? "[name].[contenthash].js" : "[name].[hash].js",
+    path: __dirname + "/dist",
+  },
+
+  resolve: {
+    extensions: [
+      ".ts",
+      ".tsx",
+      ".js",
+      ".json",
+    ],
+  },
+
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      })
+    ],
+    runtimeChunk: "single",
+    splitChunks: {
+      chunks: "all",
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: (module) => {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            return `vendor.${packageName.replace("@", "")}`;
+          },
+        },
+      },
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        loader: "file-loader",
+        options: {
+          outputPath: "assets/",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+
+  devServer: {
+    historyApiFallback: true,
+  }
 };
