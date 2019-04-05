cd2824ffe4c971cb869f1a187e522626854633bb
Michael Neu <git@michaeln.eu>
2019-04-05 15:04:09 +0200

    add in-memory test database

--- backend/test/services/mock/database.ts
+++ backend/test/services/mock/database.ts
@@ -1,3 +1,5 @@
+import { join } from "path";
+import { Connection, createConnection, Repository } from "typeorm";
 import { MockedService } from ".";
 import { IDatabaseService } from "../../../src/services/database";
 
@@ -5,5 +7,24 @@ export const MockDatabaseService = jest.fn(() =>
   new MockedService<IDatabaseService>({
     bootstrap: jest.fn(),
     getRepository: jest.fn(),
-  })
+  }),
 );
+
+export class TestDatabaseService implements IDatabaseService {
+  private _connection?: Connection;
+
+  public getRepository<T>(entity: new () => T): Repository<T> {
+    return this._connection!.getRepository<T>(entity);
+  }
+
+  public async bootstrap(): Promise<void> {
+    this._connection = await createConnection({
+      database: ":memory:",
+      entities: [
+        join(__dirname, "../../../src/entities/*"),
+      ],
+      synchronize: true,
+      type: "sqlite",
+    });
+  }
+}
