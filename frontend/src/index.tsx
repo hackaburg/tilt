b575d85dad7a879239684fe907b44eecc8000317
Michael Neu <git@michaeln.eu>
2019-04-06 19:53:42 +0200

    add react-router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { App } from "./components/app";
 
 const container = document.getElementById("app");
 const app = (
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 ReactDOM.render(app, container);
