14c564f10152ba1bbf82cf93d4f98c7540902558
Michael Neu <git@michaeln.eu>
2019-04-06 20:20:20 +0200

    add redux devtools

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,9 +5,15 @@ import { BrowserRouter } from "react-router-dom";
 import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import { App } from "./components/app";
+import { isProductionEnabled } from "./config";
 import { rootReducer } from "./reducers";
 
-const middleware = compose(applyMiddleware(thunk));
+declare var window: {
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: (...args: any[]) => any;
+};
+
+const composeEnhancers = (!isProductionEnabled && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const middleware = composeEnhancers(applyMiddleware(thunk));
 const store = createStore(rootReducer, middleware);
 const container = document.getElementById("app");
 const app = (
