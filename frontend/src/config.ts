b501a590a51ef6896088030b05a80b9edd267f0d
Michael Neu <git@michaeln.eu>
2019-04-08 00:20:37 +0200

    add css config values

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -7,3 +7,13 @@ export const isProductionEnabled = process.env.NODE_ENV === "production";
  * The base url to a tilt backend.
  */
 export const apiBaseUrl = process.env.API_BASE_URL as string;
+
+/**
+ * The default transition duration.
+ */
+export const transitionDuration = "0.2s";
+
+/**
+ * The default border radius.
+ */
+export const borderRadius = "5px";
