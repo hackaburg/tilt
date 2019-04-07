bf39dcd441ce96dcec4dd7050ad125cc3cdc55cf
Michael Neu <git@michaeln.eu>
2019-04-08 00:20:18 +0200

    add headings

---
+++ frontend/src/components/headings.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+/**
+ * A heading used to head something.
+ */
+export const Heading = styled.h1`
+  font-size: 1.5rem;
+`;
+
+/**
+ * A subheading, can be used to structure things after a @see Header.
+ */
+export const Subheading = styled.h2`
+  font-size: 1.25rem;
+  font-weight: normal;
+`;
+
+/**
+ * Highlight something in a heading.
+ */
+export const Highlight = styled.b``;
