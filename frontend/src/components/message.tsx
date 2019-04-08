1ab1d7da997afac4ee0af54d0bf33b5c2916ee20
Michael Neu <git@michaeln.eu>
2019-04-08 22:29:47 +0200

    add message component

---
+++ frontend/src/components/message.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { borderRadius } from "../config";
+
+interface IMessageProps {
+  error?: boolean;
+  warn?: boolean;
+}
+
+/**
+ * A message to display some text.
+ */
+export const Message = styled.div<IMessageProps>`
+  padding: 0.5rem;
+
+  border: 1px solid #ccc;
+  border-radius: ${borderRadius};
+  background-color: #f7f7f7;
+
+  box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.05);
+
+  ${(props) => props.error && `
+    color: #721c24;
+    background-color: #f8d7da;
+    border-color: #f5c6cb;
+  `}
+
+  ${(props) => props.warn && `
+    color: #856404;
+    background-color: #fff3cd;
+    border-color: #ffeeba;
+  `}
+`;
---
+++ frontend/test/components/message.spec.tsx
@@ -0,0 +1,21 @@
+import { shallow } from "enzyme";
+import * as React from "react";
+import { Message } from "../../src/components/message";
+
+describe("Message", () => {
+  it("renders error messages", () => {
+    const element = shallow((
+      <Message error>text</Message>
+    ));
+
+    expect(element).toMatchSnapshot();
+  });
+
+  it("renders warning messages", () => {
+    const element = shallow((
+      <Message warn>text</Message>
+    ));
+
+    expect(element).toMatchSnapshot();
+  });
+});
