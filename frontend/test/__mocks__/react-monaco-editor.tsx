9a24c1041c024e2583aadb25b0ebd09ac75a90b8
Michael Neu <git@michaeln.eu>
2019-04-20 20:43:29 +0200

    mock react-monaco-editor with simple component

---
+++ frontend/test/__mocks__/react-monaco-editor.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+/**
+ * Provide a simple component, since `react-monaco-editor` uses ES modules, which
+ * jest can't preprocess without babel.
+ */
+export default () => (
+  <div />
+);
