6a272fc9f3055c3717066dca95f69d46878400a8
Michael Neu <git@michaeln.eu>
2019-04-25 10:56:45 +0200

    add select field

---
+++ frontend/src/components/select.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import { transitionDuration } from "../config";
+import { FormField, IPlaceholderAwareProps } from "./form-field";
+
+const Field = styled.select<IPlaceholderAwareProps>`
+  width: 100%;
+  padding: 0.75rem 0rem;
+
+  -webkit-appearance: none;
+  background-color: transparent;
+  border: none;
+  font-size: 14px;
+
+  opacity: 1;
+  transition-property: opacity;
+  transition-duration: ${transitionDuration};
+
+  ${({ empty }) => empty && `
+    opacity: 0;
+  `}
+`;
+
+interface ISelectProps {
+  values: string[];
+  value: string;
+  onChange: (value: string) => any;
+  title?: string;
+  placeholder?: string;
+}
+
+/**
+ * A select dropdown.
+ */
+export const Select = ({ values, value, onChange, title, placeholder }: ISelectProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const isEmpty = !value || value === placeholder;
+  const options = values.map((optionValue) => (
+    <option key={optionValue} value={optionValue}>{optionValue}</option>
+  ));
+
+  return (
+    <FormField
+      active={isFocused && !isEmpty}
+      empty={isEmpty}
+      title={title}
+    >
+      <Field
+        active={isFocused}
+        empty={isEmpty}
+
+        value={value}
+        onChange={(event) => onChange(event.target.value)}
+
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      >
+        {placeholder && (
+          <option>{placeholder}</option>
+        )}
+
+        {options}
+      </Field>
+    </FormField>
+  );
+};
