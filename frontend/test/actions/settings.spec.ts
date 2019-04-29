5755f6acdaaaa70db99a23c78809b4c1271b2d8b
Michael Neu <git@michaeln.eu>
2019-04-29 12:02:59 +0200

    fix and add new tests to settings actions

--- frontend/test/actions/settings.spec.ts
+++ frontend/test/actions/settings.spec.ts
@@ -1,12 +1,16 @@
+import { advanceTo } from "jest-date-mock";
+import { setFormType } from "../../src/actions/form";
 import { finishRequest, startRequest } from "../../src/actions/request";
-import { fetchSettings, setSettings } from "../../src/actions/settings";
+import { fetchSettings, setSettings, updateSettings } from "../../src/actions/settings";
 import { StaticApi } from "../../src/api/static-api";
+import { FormType } from "../../src/state";
 
 describe("settings actions", () => {
   const api = new StaticApi();
 
   it("asynchronously fetches the settings", async () => {
     expect.assertions(3);
+    advanceTo(new Date());
 
     const settings = await api.getSettings();
     const dispatch = jest.fn();
@@ -15,6 +19,20 @@ describe("settings actions", () => {
 
     expect(dispatch).toBeCalledWith(startRequest());
     expect(dispatch).toBeCalledWith(setSettings(settings));
+    expect(dispatch).toBeCalledWith(finishRequest());
+  });
+
+  it("asynchronously updates the settings", async () => {
+    expect.assertions(3);
+
+    const dispatch = jest.fn();
+    const form = FormType.ApplicationSettings;
+    const settings = {};
+    const actions = updateSettings(form, settings);
+    await actions(dispatch);
+
+    expect(dispatch).toBeCalledWith(startRequest());
+    expect(dispatch).toBeCalledWith(setFormType(form));
     expect(dispatch).toBeCalledWith(finishRequest());
   });
 });
