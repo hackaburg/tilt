cd9756d45b64339a32a4e3b2fe1d89272d88ec4b
Michael Neu <git@michaeln.eu>
2019-05-02 20:24:25 +0200

    connect ws client to redux store

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,8 +5,10 @@ import { BrowserRouter } from "react-router-dom";
 import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import { RoutedConnectedApp } from "./components/app";
-import { isProductionEnabled } from "./config";
+import { apiBaseUrl, isProductionEnabled } from "./config";
 import { rootReducer } from "./reducers";
+import { WebSocketHandler } from "./ws";
+import { ActivityWebSocketHandler } from "./ws/activity-handler";
 
 declare var window: {
   __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: (...args: any[]) => any;
@@ -15,6 +17,20 @@ declare var window: {
 const composeEnhancers = (!isProductionEnabled && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 const middleware = composeEnhancers(applyMiddleware(thunk));
 const store = createStore(rootReducer, middleware);
+
+let ws: WebSocketHandler | null = null;
+store.subscribe(() => {
+  const { role } = store.getState();
+
+  if (role && !ws) {
+    ws = new WebSocketHandler(apiBaseUrl);
+    ws.registerMessageHandler(new ActivityWebSocketHandler((action) => store.dispatch(action)));
+  } else if (!role && ws) {
+    ws.close();
+    ws = null;
+  }
+});
+
 const container = document.getElementById("app");
 const app = (
   <Provider store={store}>
