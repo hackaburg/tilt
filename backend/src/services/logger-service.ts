624d2924a20eef32ca42cd6a19de6fb5769653e6
Michael Neu <git@michaeln.eu>
2019-09-15 20:26:53 +0100

    add source to logger

--- backend/src/services/logger-service.ts
+++ backend/src/services/logger-service.ts
@@ -1,19 +1,14 @@
+import { relative } from "path";
 import { Inject, Service, Token } from "typedi";
 import { createLogger, format, Logger, transports } from "winston";
 import { IService } from ".";
 import { ConfigurationServiceToken, IConfigurationService } from "./config-service";
 
-const loggerFormat = format.printf(({ level, message, ...meta }) => {
-  const timestamp = new Date().toISOString();
-  const logMessage = `[${timestamp}] [${level}]\t${message}`;
-
-  if (Object.keys(meta).length > 0) {
-    const json = JSON.stringify(meta);
-    return `${logMessage} ${json}`;
-  }
-
-  return logMessage;
-});
+interface ICallerInformation {
+  file: string;
+  line: number;
+  function: string;
+}
 
 /**
  * An interface describing the logger service.
@@ -55,9 +50,56 @@ export class LoggerService implements ILoggerService {
   ) { }
 
   /**
+   * Gets the information of the function calling the logger.
+   */
+  private getCallerInformation(): ICallerInformation {
+    const stack = new Error().stack;
+    const unknownInfo: ICallerInformation = {
+      file: "unknown",
+      function: "unknown",
+      line: 0,
+    };
+
+    if (!stack) {
+      return unknownInfo;
+    }
+
+    const callerLine = stack.split("\n")[3];
+
+    if (!callerLine) {
+      return unknownInfo;
+    }
+
+    const info = /at (\S+) \(([^\:]+):(\d+)/g.exec(callerLine);
+
+    if (!info) {
+      return unknownInfo;
+    }
+
+    return {
+      file: relative(process.cwd(), info[2]),
+      function: info[1],
+      line: Number(info[3]),
+    };
+  }
+
+  /**
    * Sets up the logger service.
    */
   public async bootstrap(): Promise<void> {
+    const loggerFormat = format.printf(({ level, message, ...args }) => {
+      const timestamp = new Date().toISOString();
+      const { meta, caller } = args;
+      const logMessage = `[${timestamp}] [${level}] [${caller.file}:${caller.line}] [${caller.function}] ${message}`;
+
+      if (Object.keys(meta).length > 0) {
+        const json = JSON.stringify(meta);
+        return `${logMessage} ${json}`;
+      }
+
+      return logMessage;
+    });
+
     this._logger = createLogger({
       level: this._config.config.log.level,
       transports: [
@@ -82,7 +124,10 @@ export class LoggerService implements ILoggerService {
    */
   public debug(message: string, ...meta: any[]) {
     if (this._logger) {
-      this._logger.debug(message, ...meta);
+      this._logger.debug(message, {
+        caller: this.getCallerInformation(),
+        meta,
+      });
     }
   }
 
@@ -93,7 +138,10 @@ export class LoggerService implements ILoggerService {
    */
   public info(message: string, ...meta: any[]) {
     if (this._logger) {
-      this._logger.info(message, ...meta);
+      this._logger.info(message, {
+        caller: this.getCallerInformation(),
+        meta,
+      });
     }
   }
 
@@ -104,7 +152,10 @@ export class LoggerService implements ILoggerService {
    */
   public error(message: string, ...meta: any[]) {
     if (this._logger) {
-      this._logger.error(message, ...meta);
+      this._logger.error(message, {
+        caller: this.getCallerInformation(),
+        meta,
+      });
     }
   }
 }
---
+++ backend/test/services/logger-service.spec.ts
@@ -0,0 +1,131 @@
+import { IConfigurationService } from "../../src/services/config-service";
+import { ILoggerService, LoggerService } from "../../src/services/logger-service";
+import { MockedService } from "./mock";
+import { MockConfigurationService } from "./mock/mock-config-service";
+
+interface IMockedLogger {
+  debug: jest.Mock<any, any[]>;
+  info: jest.Mock<any, any[]>;
+  error: jest.Mock<any, any[]>;
+}
+
+interface IMockedWinston {
+  createLogger: jest.Mock<IMockedLogger>;
+  format: {
+    colorize: jest.Mock;
+    combine: jest.Mock;
+    printf: jest.Mock<any, [(info: any) => string]>;
+  };
+  transports: {
+    Console: jest.Mock;
+    File: jest.Mock;
+  };
+}
+
+jest.mock("winston", jest.fn(() => ({
+  createLogger: jest.fn(),
+  format: {
+    colorize: jest.fn(),
+    combine: jest.fn(),
+    printf: jest.fn(),
+  },
+  transports: {
+    Console: jest.fn(),
+    File: jest.fn(),
+  },
+} as IMockedWinston)));
+
+describe("LoggerService", () => {
+  let config: MockedService<IConfigurationService>;
+  let service: ILoggerService;
+  let winston: IMockedWinston;
+
+  beforeEach(() => {
+    winston = require("winston");
+
+    config = new MockConfigurationService({
+      config: {
+        log: {
+          filename: "log.log",
+          level: "verbose",
+        },
+      },
+    } as any);
+
+    service = new LoggerService(config.instance);
+  });
+
+  it("sets up winston", async () => {
+    expect.assertions(1);
+    await service.bootstrap();
+    expect(winston.createLogger).toBeCalled();
+  });
+
+  it("logs to both file and console", async () => {
+    expect.assertions(2);
+    await service.bootstrap();
+    expect(winston.transports.File).toBeCalled();
+    expect(winston.transports.Console).toBeCalled();
+  });
+
+  it("creates a custom log format", async () => {
+    expect.assertions(5);
+    let messageTransformer: ((info: any) => string) | undefined;
+    winston.format.printf.mockImplementation((callback) => messageTransformer = callback);
+    await service.bootstrap();
+
+    expect(winston.format.printf).toBeCalled();
+    expect(winston.format.colorize).toBeCalled();
+
+    if (messageTransformer) {
+      const logMessage = "test";
+      const logLevel = "info";
+      const messageWithoutData = messageTransformer({
+        caller: {},
+        level: logLevel,
+        message: logMessage,
+        meta: {},
+      });
+
+      expect(messageWithoutData).toContain(logMessage);
+      expect(messageWithoutData).toContain(logLevel);
+
+      const data = {
+        value: 10,
+      };
+      const messageWithData = messageTransformer({
+        caller: {},
+        level: logLevel,
+        message: logMessage,
+        meta: data,
+      });
+      expect(messageWithData).toContain(JSON.stringify(data));
+    }
+  });
+
+  it("logs", async () => {
+    expect.assertions(3);
+    const debug = jest.fn();
+    const info = jest.fn();
+    const error = jest.fn();
+
+    winston.createLogger.mockImplementation(() => ({
+      debug,
+      error,
+      info,
+    }));
+    await service.bootstrap();
+
+    const debugMessage = "debug";
+    service.debug(debugMessage);
+    expect(debug).toBeCalledWith(debugMessage, expect.anything());
+
+    const infoMessage = "info";
+    service.info(infoMessage);
+    expect(info).toBeCalledWith(infoMessage, expect.anything());
+
+    const errorMessage = "error";
+    service.error(errorMessage);
+    expect(error).toBeCalledWith(errorMessage, expect.anything());
+  });
+});
