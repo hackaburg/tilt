760ddf75f434be1c730320c7026b29e5c7a48036
Bastian Meissner <meissner.bastian@gmail.com>
2019-09-27 21:09:27 +0200

    Added Header Bar

--- frontend/src/components/headerbar.tsx
+++ frontend/src/components/headerbar.tsx
@@ -10,21 +10,23 @@ export const HeaderBar = styled.div<ISidebarAwareProps>`
   display: flex;
 
   top: 0px;
-  left: 0px;
-  width: 100%;
-  height: ${ headerBarHeight };
 
   z-index: 1;
+
+  transition-property: left;
+  transition-duration: ${transitionDuration};
+  justify-content: center;
 
   background-color: white;
   box-shadow: 0 4px 2px -2px gray;
 
-  flex-direction: row;
   align-items: center;
   justify-content: center;
 
-  transition-property: left;
-  transition-duration: ${ transitionDuration };
+  width: 100vw;
+  height: ${ headerBarHeight };
+
+  flex-direction: row;
 
   ${(props) => props.showSidebar && `
     left: ${sidebarWidth};
