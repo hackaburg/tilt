4e4f5994245a151d715cb1595ee93ce39b4dd346
Michael Neu <git@michaeln.eu>
2019-12-25 22:13:19 +0100

    introduce settings context

---
+++ frontend/src/contexts/settings-context.tsx
@@ -0,0 +1,82 @@
+import * as React from "react";
+import { useCallback, useMemo, useState } from "react";
+import { ISettings } from "../../../types/settings";
+import { useApi } from "../hooks/use-api";
+import { useContextOrThrow } from "../hooks/use-context-or-throw";
+import { Nullable } from "../state";
+
+interface ISettingsContextValue {
+  isFetchingSettings: boolean;
+  settings: ISettings;
+  updateSettings: (settings: ISettings) => void;
+  updateError: Nullable<Error>;
+}
+
+const Context = React.createContext<Nullable<ISettingsContextValue>>(null);
+Context.displayName = "SettingsContext";
+
+interface ISettingsContextProviderProps {
+  children: React.ReactNode;
+}
+
+/**
+ * A context storing and loading the application settings
+ * @param props The props to pass to this context
+ */
+export const SettingsContextProvider = ({
+  children,
+}: ISettingsContextProviderProps) => {
+  const [localSettings, setLocalSettings] = useState<Nullable<ISettings>>(null);
+  const [
+    fetchedSettings,
+    isFetchingSettings,
+    fetchError,
+  ] = useApi(async (api) => api.getSettings());
+
+  if (fetchError) {
+    throw fetchError;
+  }
+
+  if (!isFetchingSettings && fetchedSettings == null) {
+    throw new Error("No settings received from the server");
+  }
+
+  const updateSettings = useCallback((settings: ISettings) => {
+    if (settings != null) {
+      setLocalSettings(settings);
+    }
+  }, []);
+
+  const [, isUpdatingSettings, updateError] = useApi(
+    async (api) => {
+      if (localSettings != null) {
+        await api.updateSettings(localSettings);
+      }
+    },
+    [localSettings],
+  );
+
+  const value = useMemo<ISettingsContextValue>(
+    () => ({
+      isFetchingSettings: isFetchingSettings || isUpdatingSettings,
+      settings: localSettings ?? (fetchedSettings as ISettings),
+      updateError,
+      updateSettings,
+    }),
+    [
+      isFetchingSettings,
+      isUpdatingSettings,
+      localSettings,
+      fetchedSettings,
+      updateSettings,
+      updateError,
+    ],
+  );
+
+  return <Context.Provider value={value}>{children}</Context.Provider>;
+};
+
+/**
+ * Gets the settings context's value.
+ */
+export const useSettingsContext = () => useContextOrThrow(Context);
