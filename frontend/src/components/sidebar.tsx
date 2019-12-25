76e8a89033b316afb0cb6d71e967701be76bb59f
Michael Neu <git@michaeln.eu>
2019-12-25 19:11:54 +0100

    use login context in sidebar

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,11 +1,8 @@
 import * as React from "react";
-import { connect } from "react-redux";
-import { bindActionCreators, Dispatch } from "redux";
 import styled from "styled-components";
 import { UserRole } from "../../../types/roles";
-import { logout as logoutRaw } from "../actions/login";
+import { useLoginContext } from "../contexts/login-context";
 import { Routes } from "../routes";
-import { IState } from "../state";
 import { ConnectedSidebarLogo } from "./sidebar-logo";
 import { SidebarMenu, SidebarMenuItem } from "./sidebar-menu";
 
@@ -32,59 +29,44 @@ const Container = styled.div`
   }
 `;
 
-interface ISidebarProps {
-  role: UserRole;
-  logout: typeof logoutRaw;
-}
-
 /**
  * The sidebar containing the menu and a logo
  */
-export const Sidebar = ({ role, logout }: ISidebarProps) => (
-  <Container>
-    <ConnectedSidebarLogo />
+export const Sidebar = () => {
+  const loginState = useLoginContext();
 
-    <SidebarMenu>
-      <SidebarMenuItem to={Routes.Dashboard}>Dashboard</SidebarMenuItem>
+  if (!loginState.isLoggedIn) {
+    return null;
+  }
 
-      {[UserRole.Moderator, UserRole.Owner].includes(role) && (
-        <>
-          <SidebarMenuItem to={Routes.Users}>Users</SidebarMenuItem>
-          <SidebarMenuItem to={Routes.Statistics}>Statistics</SidebarMenuItem>
-        </>
-      )}
+  const { role, logout } = loginState;
+  const isElevatedUser = [UserRole.Moderator, UserRole.Owner].includes(role);
 
-      {role === UserRole.Owner && (
-        <>
-          <SidebarMenuItem to={Routes.Activity}>Activity</SidebarMenuItem>
-          <SidebarMenuItem to={Routes.Settings}>Settings</SidebarMenuItem>
-        </>
-      )}
+  return (
+    <Container>
+      <ConnectedSidebarLogo />
 
-      <SidebarMenuItem to={Routes.Logout} onClick={logout}>
-        Logout
-      </SidebarMenuItem>
-    </SidebarMenu>
-  </Container>
-);
+      <SidebarMenu>
+        <SidebarMenuItem to={Routes.Dashboard}>Dashboard</SidebarMenuItem>
 
-const mapStateToProps = (state: IState) => ({
-  role: state.role || UserRole.User,
-});
+        {isElevatedUser && (
+          <>
+            <SidebarMenuItem to={Routes.Users}>Users</SidebarMenuItem>
+            <SidebarMenuItem to={Routes.Statistics}>Statistics</SidebarMenuItem>
+          </>
+        )}
 
-const mapDispatchToProps = (dispatch: Dispatch) => {
-  return bindActionCreators(
-    {
-      logout: logoutRaw,
-    },
-    dispatch,
+        {role === UserRole.Owner && (
+          <>
+            <SidebarMenuItem to={Routes.Activity}>Activity</SidebarMenuItem>
+            <SidebarMenuItem to={Routes.Settings}>Settings</SidebarMenuItem>
+          </>
+        )}
+
+        <SidebarMenuItem to={Routes.Logout} onClick={logout}>
+          Logout
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </Container>
   );
 };
-
-/**
- * The sidebar connected to the redux store.
- */
-export const ConnectedSidebar = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Sidebar);
