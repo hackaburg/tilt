fdf24944f66aa2d0f9f1741e31224bb0f7e61fcf
Michael Neu <git@michaeln.eu>
2019-12-26 00:00:50 +0100

    remove redux

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,26 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { applyMiddleware, compose, createStore } from "redux";
-import thunk from "redux-thunk";
 import { RoutedApp } from "./components/app";
-import { isProductionEnabled } from "./config";
 import { LoginContextProvider } from "./contexts/login-context";
 import { NotificationContextProvider } from "./contexts/notification-context";
 import { SettingsContextProvider } from "./contexts/settings-context";
 import { WebSocketContextProvider } from "./contexts/ws-context";
-import { rootReducer } from "./reducers";
-
-declare var window: {
-  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: (...args: any[]) => any;
-};
-
-const composeEnhancers =
-  (!isProductionEnabled && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
-const middleware = composeEnhancers(applyMiddleware(thunk));
-const store = createStore(rootReducer, middleware);
 
 const container = document.getElementById("app");
 const app = (
@@ -28,11 +13,9 @@ const app = (
     <SettingsContextProvider>
       <LoginContextProvider>
         <WebSocketContextProvider>
-          <Provider store={store}>
-            <BrowserRouter>
-              <RoutedApp />
-            </BrowserRouter>
-          </Provider>
+          <BrowserRouter>
+            <RoutedApp />
+          </BrowserRouter>
         </WebSocketContextProvider>
       </LoginContextProvider>
     </SettingsContextProvider>
