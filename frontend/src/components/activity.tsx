9f56bbc515fdb9782d0959a0378c0918882bf345
Michael Neu <git@michaeln.eu>
2019-12-25 23:52:03 +0100

    use ws context for activities

--- frontend/src/components/activity.tsx
+++ frontend/src/components/activity.tsx
@@ -1,46 +1,23 @@
 import * as React from "react";
-import { useEffect } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators, Dispatch } from "redux";
-import { IActivity } from "../../../types/activity";
-import { fetchActivities } from "../actions/activity";
-import { IState } from "../state";
+import { useActivity } from "../hooks/use-activity";
 import { groupByMonth } from "../util";
 import { ActivityEvent } from "./activity-event";
 import { Heading, Subheading } from "./headings";
 import { Placeholder } from "./placeholder";
 
-interface IActivityProps {
-  activity: IActivity[] | null;
-  dispatchFetchActivities: typeof fetchActivities;
-}
-
 /**
  * An overview over activities in tilt.
  */
-export const Activity = ({
-  activity,
-  dispatchFetchActivities,
-}: IActivityProps) => {
-  useEffect(() => {
-    if (!activity) {
-      dispatchFetchActivities();
-    }
-  }, []);
-
-  const descendingActivities = (activity || []).sort(
-    (a, b) => b.timestamp - a.timestamp,
-  );
-  const activityByMonth = groupByMonth(descendingActivities).map(
-    ({ month, data }) => (
-      <div key={month}>
-        <Subheading>{month}</Subheading>
-        {data.map((event) => (
-          <ActivityEvent event={event} key={JSON.stringify(event)} />
-        ))}
-      </div>
-    ),
-  );
+export const Activity = () => {
+  const activity = useActivity();
+  const activityByMonth = groupByMonth(activity).map(({ month, data }) => (
+    <div key={month}>
+      <Subheading>{month}</Subheading>
+      {data.map((event) => (
+        <ActivityEvent event={event} key={JSON.stringify(event)} />
+      ))}
+    </div>
+  ));
 
   return (
     <>
@@ -58,24 +35,3 @@ export const Activity = ({
     </>
   );
 };
-
-const mapStateToProps = (state: IState) => ({
-  activity: state.activity,
-});
-
-const mapDispatchToProps = (dispatch: Dispatch) => {
-  return bindActionCreators(
-    {
-      dispatchFetchActivities: fetchActivities,
-    },
-    dispatch,
-  );
-};
-
-/**
- * The activity component connected to the redux store.
- */
-export const ConnectedActivity = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Activity);
