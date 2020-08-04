6f440ce41f567199cb63da72dac254aa19545e53
Michael Neu <git@michaeln.eu>
2020-08-04 23:03:07 +0200

    replace uuid with counter to reduce bundle size

--- frontend/src/hooks/use-uniqe-id.ts
+++ frontend/src/hooks/use-uniqe-id.ts
@@ -1,21 +1,35 @@
-import { v4 as uuid } from "node-uuid";
 import { useRef } from "react";
+
+let counter = 0;
+
+// it's safe to assume this won't overflow
+const incrementAndGetCounter = () => ++counter;
+
+const generateUniqueID = () => `unique-id-${incrementAndGetCounter()}`;
 
 /**
  * Generates a unique id.
  */
-export const useUniqueID = () => {
-  const { current: id } = useRef(uuid());
-  return id;
+export const useUniqueID = (): string => {
+  const ref = useRef<string | null>(null);
+
+  if (ref.current == null) {
+    ref.current = generateUniqueID();
+  }
+
+  return ref.current as string;
 };
 
 /**
  * Generates an array of unique ids.
  * @param count The amount of ids to generate
  */
-export const useUniqueIDs = (count: number) => {
-  const idArray = new Array(count).fill(0).map(() => uuid());
-  const { current: ids } = useRef(idArray);
+export const useUniqueIDs = (count: number): readonly string[] => {
+  const ref = useRef<readonly string[] | null>(null);
 
-  return ids;
+  if (ref.current == null) {
+    ref.current = new Array(count).fill(0).map(() => generateUniqueID());
+  }
+
+  return ref.current as readonly string[];
 };
