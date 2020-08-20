eced5b6e496e656710ec51fc3bed839feb8cea20
Michael Neu <git@michaeln.eu>
2020-08-20 18:03:15 +0200

    extract copyable text to component

---
+++ frontend/src/components/base/copyable-text.tsx
@@ -0,0 +1,63 @@
+import styled from "@emotion/styled";
+import * as React from "react";
+import { useCallback, useRef } from "react";
+import { useNotificationContext } from "../../contexts/notification-context";
+import { Nullable } from "../../state";
+import { Button } from "./button";
+import { Elevated } from "./elevated";
+import { FlexColumnContainer } from "./flex";
+
+const ScrollableText = styled.textarea`
+  resize: none;
+  padding: 1rem;
+  font-family: Monaco, Consolas, Inconsolata, monospace;
+  font-size: inherit;
+  border: none;
+  height: 200px;
+`;
+
+const ignoreChange = () => 0;
+
+interface ICopyableTextProps {
+  text: string;
+}
+
+/**
+ * A textfield with a button to allow easy copy and paste.
+ */
+export const CopyableText = ({ text }: ICopyableTextProps) => {
+  const { showNotification } = useNotificationContext();
+  const ref = useRef<Nullable<HTMLTextAreaElement>>(null);
+
+  const handleCopy = useCallback(() => {
+    const { current: area } = ref;
+
+    if (area == null) {
+      return;
+    }
+
+    area.select();
+    area.setSelectionRange(0, text.length);
+    document.execCommand("copy");
+    area.setSelectionRange(0, 0);
+
+    showNotification("Copied");
+  }, [showNotification, text]);
+
+  return (
+    <FlexColumnContainer>
+      <Elevated level={1}>
+        <ScrollableText
+          ref={ref}
+          value={text}
+          onChange={ignoreChange}
+          disabled
+        />
+      </Elevated>
+
+      <Button onClick={handleCopy} primary>
+        Copy to clipboard
+      </Button>
+    </FlexColumnContainer>
+  );
+};
--- frontend/src/components/base/error-boundary.tsx
+++ frontend/src/components/base/error-boundary.tsx
@@ -1,10 +1,7 @@
 import styled from "@emotion/styled";
 import * as React from "react";
-import { useCallback, useRef } from "react";
-import { useNotificationContext } from "../../contexts/notification-context";
 import { Nullable } from "../../state";
-import { Button } from "./button";
-import { Elevated } from "./elevated";
+import { CopyableText } from "./copyable-text";
 import {
   CenteredContainer,
   PageSizedContainer,
@@ -14,83 +11,14 @@ import { Heading } from "./headings";
 import { ExternalLink } from "./link";
 import { Text } from "./text";
 
-const ErrorCode = styled.textarea`
-  resize: none;
-  padding: 1rem;
-  font-family: Monaco, Consolas, Inconsolata, monospace;
-  font-size: inherit;
-  border: none;
-  height: 200px;
-`;
-
 const ErrorContainer = styled(StyleableFlexContainer)`
-  width: 320px;
+  width: 300px;
 `;
-
-const ignoreChange = () => 0;
 
 interface IRenderError {
   exception: Error;
   info: React.ErrorInfo;
 }
-
-const RenderError = ({ exception, info }: IRenderError) => {
-  const { showNotification } = useNotificationContext();
-  const message = `${exception.name}: ${exception.message}\n${exception.stack}\n\nComponent stack:${info.componentStack}`;
-  const ref = useRef<Nullable<HTMLTextAreaElement>>(null);
-
-  const handleCopy = useCallback(() => {
-    const { current: area } = ref;
-
-    if (area == null) {
-      return;
-    }
-
-    area.select();
-    area.setSelectionRange(0, message.length);
-    document.execCommand("copy");
-    area.setSelectionRange(0, 0);
-
-    showNotification("Copied");
-  }, [showNotification]);
-
-  return (
-    <PageSizedContainer>
-      <CenteredContainer>
-        <ErrorContainer>
-          <Heading text="Uh-oh, everything is on fire 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥" />
-
-          <Text>
-            While this shouldn't have happened in the first place, we'd be happy
-            if you report this error on our{" "}
-            <ExternalLink to="https://github.com/hackaburg/tilt/issues/new">
-              issue tracker
-            </ExternalLink>
-            .
-          </Text>
-
-          <Text>
-            You can include this error log along a quick description to
-            reproduce this issue:
-          </Text>
-
-          <Elevated level={1}>
-            <ErrorCode
-              ref={ref}
-              value={message}
-              onChange={ignoreChange}
-              disabled
-            />
-          </Elevated>
-
-          <Button onClick={handleCopy} primary>
-            Copy to clipboard
-          </Button>
-        </ErrorContainer>
-      </CenteredContainer>
-    </PageSizedContainer>
-  );
-};
 
 interface IState {
   error: Nullable<IRenderError>;
@@ -125,7 +53,33 @@ export class ErrorBoundary extends React.Component<IProps, IState> {
 
     if (error != null) {
       const { exception, info } = error;
-      return <RenderError exception={exception} info={info} />;
+      const message = `${exception.name}: ${exception.message}\n${exception.stack}\n\nComponent stack:${info.componentStack}`;
+
+      return (
+        <PageSizedContainer>
+          <CenteredContainer>
+            <ErrorContainer>
+              <Heading text="Uh-oh, everything is on fire 🔥🔥🔥🔥🔥🔥🔥🔥🔥" />
+
+              <Text>
+                While this shouldn't have happened in the first place, we'd be
+                happy if you report this error on our{" "}
+                <ExternalLink to="https://github.com/hackaburg/tilt/issues/new">
+                  issue tracker
+                </ExternalLink>
+                .
+              </Text>
+
+              <Text>
+                You can include this error log along a quick description to
+                reproduce this issue:
+              </Text>
+
+              <CopyableText text={message} />
+            </ErrorContainer>
+          </CenteredContainer>
+        </PageSizedContainer>
+      );
     }
 
     return this.props.children;
