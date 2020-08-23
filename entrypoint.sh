5079f119d14ae6d0d858ccba5682bd25f6b1e99d
Michael Neu <git@michaeln.eu>
2020-08-23 12:08:00 +0200

    replace base url disregarding in-tag whitespace

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # this change only exists as long as the container exists, which will eventually
 # be re-created. the base image is not affected by this replacement
 echo "[*] setting base url to '$BASE_URL'"
-sed -i 's|<base href="" />|<base href="'$BASE_URL'" />|' frontend/index.html
+sed -i 's|<base href=""\s*/>|<base href="'$BASE_URL'"/>|' frontend/index.html
 
 echo "[*] starting tilt"
 node backend/tilt.js
