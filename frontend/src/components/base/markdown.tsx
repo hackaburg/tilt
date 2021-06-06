82f22835781460d3503343ecf6c9685ba027a4a0
phntxx <meissner.bastian@gmail.com>
2021-06-06 19:24:37 +0200

    Fix Markdown

--- frontend/src/components/base/markdown.tsx
+++ frontend/src/components/base/markdown.tsx
@@ -4,36 +4,23 @@ import { FlexColumnContainer } from "./flex";
 import { Heading, Subheading, Subsubheading } from "./headings";
 import { Image } from "./image";
 import { Text } from "./text";
-
-const renderers: ReactMarkdown.Renderers = {
-  ...ReactMarkdown.renderers,
-  heading: ({ children, level }) => {
-    switch (level) {
-      case 1:
-        return <Heading text={children} />;
-
-      case 2:
-        return <Subheading text={children} />;
-    }
-
-    return <Subsubheading text={children} />;
-  },
-  image: ({ src, alt }) => <Image src={src} label={alt} />,
-  paragraph: ({ children }) => <Text>{children}</Text>,
-  root: ({ children }) => <FlexColumnContainer>{children}</FlexColumnContainer>,
+const renderers: ReactMarkdown.TransformOptions["components"] = {
+  h1: ({ children }) => <Heading text={children} />,
+  h2: ({ children }) => <Subheading text={children} />,
+  h3: ({ children }) => <Subsubheading text={children} />,
+  img: ({ src, alt }) => <Image src={src as string} label={alt as string} />,
+  p: ({ children }) => <Text>{children}</Text>,
 };
-
 interface IMarkdownProps {
   text: string;
 }
-
 /**
  * Renders markdown.
  */
 export const Markdown = ({ text }: IMarkdownProps) => (
-  <ReactMarkdown
-    source={text}
-    linkTarget="_blank"
-    renderers={renderers as any}
-  />
+  <FlexColumnContainer>
+    <ReactMarkdown linkTarget="_blank" components={renderers}>
+      {text}
+    </ReactMarkdown>
+  </FlexColumnContainer>
 );
