410ff18eb3dcc54b0087a863528c5d927b8bb65f
Markus Guder <markus.guder@gmail.com>
2023-05-12 09:40:15 +0200

    remove console.log

--- frontend/src/components/pages/admission.tsx
+++ frontend/src/components/pages/admission.tsx
@@ -683,10 +683,7 @@ export const Admission = () => {
       ...questions.map((question) => question.title),
     ];
 
-    console.log(questions);
-
     const rows = applicationsSortedByDate.map((application) => {
-      console.log(application);
       return [
         application.user.id,
         application.user.email,
