8b9805abf8601618431f3aa24d7be973e5932748
marc101101 <markus.guder@gmail.com>
2024-01-21 18:15:33 +0100

    fix: 🐛 linting error

--- frontend/src/components/base/divider.tsx
+++ frontend/src/components/base/divider.tsx
@@ -1,14 +1,16 @@
-import styled from "@emotion/styled";
 import * as React from "react";
-import { FlexColumnContainer, Spacer, StyleableFlexContainer } from "./flex";
-
-const Border = styled(StyleableFlexContainer)`
-  border-top: 1px dashed #ccc;
-`;
 
 /**
  * A vertical divider.
  */
 export const Divider = () => (
-    <hr style={{width: "3rem", marginLeft: "0", height: "5px", color: "black", backgroundColor: "black"}}></hr>
+  <hr
+    style={{
+      width: "3rem",
+      marginLeft: "0",
+      height: "5px",
+      color: "black",
+      backgroundColor: "black",
+    }}
+  ></hr>
 );
