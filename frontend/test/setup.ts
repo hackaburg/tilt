31d89f563f39b6d99f564d989bd03f2abf30dc0e
sezanzeb <28510156+sezanzeb@users.noreply.github.com>
2026-05-31 22:04:25 +0200

    chartjs breaking changes

--- frontend/test/setup.ts
+++
@@ -1,8 +0,0 @@
-import * as Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-import * as Enzyme from "enzyme";
-import "jest-date-mock";
-import "jest-enzyme";
-
-Enzyme.configure({
-  adapter: new Adapter(),
-});
